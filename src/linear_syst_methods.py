df53b6c8226047cb9645393f3ca3f9300e23d2d1
Keyaru <118556238+keyaru18@users.noreply.github.com.>
2026-01-12 12:44:14 -0500

    :sparkles: Editar las funciones descomposicion_LU y resolver_LU para aplicar permutaciones

--- src/linear_syst_methods.py
+++ src/linear_syst_methods.py
@@ -96,7 +96,7 @@ def eliminacion_gaussiana(A: np.ndarray) -> np.ndarray:
 
 
 # ####################################################################
-def descomposicion_LU(A: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def descomposicion_LU(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Realiza la descomposición LU de una matriz cuadrada A.
     [IMPORTANTE] No se realiza pivoteo.
 
@@ -119,10 +119,18 @@ def descomposicion_LU(A: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     n = A.shape[0]
 
     L = np.zeros((n, n), dtype=float)
+    P = np.identity(n, dtype=float)
 
     for i in range(0, n):  # loop por columna
 
         # --- deterimnar pivote
+        pivot_index = np.argmax(np.abs(A[i:, i])) + i
+        if pivot_index != i:
+            P[[i, pivot_index], :] = P[[pivot_index, i], :]
+            A[[i, pivot_index], :] = A[[pivot_index, i], :]
+            if i > 0:
+                L[[i, pivot_index], :i] = L[[pivot_index, i], :i]
+
         if A[i, i] == 0:
             raise ValueError("No existe solución única.")
 
@@ -139,11 +147,11 @@ def descomposicion_LU(A: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     if A[n - 1, n - 1] == 0:
         raise ValueError("No existe solución única.")
 
-    return L, A
+    return P, L, A
 
 
 # ####################################################################
-def resolver_LU(L: np.ndarray, U: np.ndarray, b: np.ndarray) -> np.ndarray:
+def resolver_LU(P: np.ndarray,L: np.ndarray, U: np.ndarray, b: np.ndarray) -> np.ndarray:
     """Resuelve un sistema de ecuaciones lineales mediante la descomposición LU.
 
     ## Parameters
@@ -161,6 +169,7 @@ def resolver_LU(L: np.ndarray, U: np.ndarray, b: np.ndarray) -> np.ndarray:
     """
 
     n = L.shape[0]
+    b = P @ b  # aplicar permutación a b
 
     # --- Sustitución hacia adelante
     logging.info("Sustitución hacia adelante")
